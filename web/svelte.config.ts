e87862116fcadf2145b952e6d6e06ba234aa4f21
BryanDGuy <doughertybryan13@gmail.com>
2025-11-14 13:53:21 -0600

    Some cleanup

---
+++ web/svelte.config.ts
@@ -0,0 +1,9 @@
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import type { Config } from '@sveltejs/vite-plugin-svelte';
+
+const config: Config = {
+  preprocess: vitePreprocess()
+};
+
+export default config;
+
