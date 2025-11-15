0da36c3bdaf416924fed02266a5b8c869521a964
BryanDGuy <doughertybryan13@gmail.com>
2025-11-15 12:23:29 -0600

    Moving rank

--- internal/card/card.go
+++ internal/card/card.go
@@ -1,23 +1,5 @@
 package card
 
-type Rank int
-
-const (
-	Two Rank = iota
-	Three
-	Four
-	Five
-	Six
-	Seven
-	Eight
-	Nine
-	Ten
-	Jack
-	Queen
-	King
-	Ace
-)
-
 type Card struct {
 	Rank Rank
 }
---
+++ internal/card/rank.go
@@ -0,0 +1,19 @@
+package card
+
+type Rank int
+
+const (
+	Two Rank = iota
+	Three
+	Four
+	Five
+	Six
+	Seven
+	Eight
+	Nine
+	Ten
+	Jack
+	Queen
+	King
+	Ace
+)
